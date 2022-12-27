08bf81bf34bc8ebdfe2eb2b1755192792728f870
farhana-akter-22 <farhanajatri@gmail.com>
2022-12-27 15:16:45 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme= require ("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
